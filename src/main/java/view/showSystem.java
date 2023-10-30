efc3fa2e68394a9dd2f3248438b5b75ceb317c76
lightkunwo <111209971+lightkunwo@users.noreply.github.com>
2023-10-30 11:17:48 +0800

    Update showSystem.java

--- src/main/java/view/showSystem.java
+++ src/main/java/view/showSystem.java
@@ -33,7 +33,7 @@ public class showSystem extends JFrame {
     public void showFrame() {
         this.setVisible(true);
         this.setTitle("经典软件体系结构教学软件");
-        this.setSize(800, 600);
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setSize(1000, 500);
